2fd2fcc38b56e48834f454546d1967f95de3515b
github username <github email address>
2018-09-28 09:35:46 -0400

    Done.

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,14 +1,35 @@
 def even_next(n)
+  n/2
 end
 
 def odd_next(n)
+  3*n + 1
 end
 
 def next_value(n)
+  if n % 2 == 0
+    even_next(n)
+  else
+    odd_next(n)
+  end
 end
 
 def collatz(n)
+  arr = [n]
+  while arr.last > 1
+    arr << next_value(arr.last)
+  end
+  arr
 end
 
 def longest_collatz
+  longest = collatz(13)
+  counter = 14
+  999986.times do
+    if collatz(counter).length > longest.length
+      longest = collatz(counter)
+    end
+    counter += 1
+  end
+  longest[0]
 end
